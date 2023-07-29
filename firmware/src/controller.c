6f8b2c9648e1fb8887db646c0ca55beb11434b5c
Guy John <guy@rumblesan.com>
2023-07-29 10:56:51 +0100

    Stop param jumping value on parameter change

--- firmware/src/controller.c
+++ firmware/src/controller.c
@@ -1,25 +1,47 @@
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "controller.h"
 
 void controller_init(Controller *c) {
   c->current = 0;
-  for (int i = 0; i < CONTROL_COUNT; i++) { c->values[i] = 0;
+  c->locked = false;
+  c->previous = 0;
+  for (int i = 0; i < CONTROL_COUNT; i++) {
+    c->values[i] = 0;
   }
 }
 
+void controller_lock(Controller *c, uint16_t previous) {
+  c->locked = true;
+  c->previous = previous;
+}
+
 void controller_next_control(Controller *c) {
+  c->locked = true;
   c->current += 1;
   if (c->current >= CONTROL_COUNT) {
     c->current = 0;
   }
 }
 
-void controller_set_current(Controller *c, int value) {
+void controller_set_current(Controller *c, uint16_t value) {
+  if (c->locked) {
+    // don't check max value as the highest value we get through
+    // from ADCs is 10bit
+    if (value < UNLOCK_THRESH ||
+        (value > (c->previous - UNLOCK_THRESH) &&
+        value < (c->previous + UNLOCK_THRESH))
+     ) {
+      return;
+    }
+    c->locked = false;
+  }
+  c->previous = value;
   controller_set_control(c, c->current, value);
 }
 
-void controller_set_control(Controller *c, int control, uint16_t value) {
+void controller_set_control(Controller *c, uint8_t control, uint16_t value) {
   switch(control) {
     case CONTROL_FM_FREQ:
       c->values[CONTROL_FM_FREQ] = value;
@@ -34,7 +56,7 @@ void controller_set_control(Controller *c, int control, uint16_t value) {
   }
 }
 
-int controller_get_control(Controller *c, int control) {
+uint16_t controller_get_control(Controller *c, uint8_t control) {
   if (control < 0 || control >= CONTROL_COUNT) { return 0; }
   return c->values[control];
 }
--- firmware/src/controller.h
+++ firmware/src/controller.h
@@ -1,23 +1,30 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define CONTROL_COUNT 3
 #define CONTROL_FM_FREQ 0
 #define CONTROL_FM_DEPTH 1
 #define CONTROL_OCTAVE 2
 
+#define UNLOCK_THRESH 50
+
 typedef struct Controller {
-  int current;
-  int values[CONTROL_COUNT];
+  bool locked;
+  uint16_t previous;
+  uint8_t current;
+  uint16_t values[CONTROL_COUNT];
 } Controller;
 
 void controller_init(Controller *c);
 
+void controller_lock(Controller *c, uint16_t previous);
+
 void controller_next_control(Controller *c);
 
-void controller_set_current(Controller *c, int value);
+void controller_set_current(Controller *c, uint16_t value);
 
-void controller_set_control(Controller *c, int control, uint16_t value);
+void controller_set_control(Controller *c, uint8_t control, uint16_t value);
 
-int controller_get_control(Controller *c, int control);
+uint16_t controller_get_control(Controller *c, uint8_t control);
