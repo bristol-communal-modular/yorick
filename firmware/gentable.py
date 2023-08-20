d2aaa4dfdf5a569bb16e1e889108fbb82266d7e5
Guy John <guy@rumblesan.com>
2023-08-20 16:49:17 +0100

    Updating wavetables

--- firmware/gentable.py
+++ firmware/gentable.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python
 
+import sys
 import math
+import random
 
-def main():
+
+def sine():
     delta = 360.0 / 256.0
 
     for i in range(256):                                                                                                       
         angle = i * delta
         rad = math.radians(angle)
         s = math.sin(rad)
-        o = round((s * 127) + 128)
+        o = round((s * 127))
+        print("  {}, // {}".format(o, i))
+
+def saw():
+    for i in range(256):                                                                                                       
+        o = i - 128
+        print("  {}, // {}".format(o, i))
+
+def square():
+    delta = 360.0 / 256.0
+
+    for i in range(256):                                                                                                       
+        if i < 128:
+            o = -128
+        else:
+            o = 127
+        print("  {}, // {}".format(o, i))
+
+def rand():
+    for i in range(256):                                                                                                       
+        o = round((random.random() * 256) - 128)
         print("  {}, // {}".format(o, i))
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        wavetype = sys.argv[1]
+    else:
+        wavetype = "sine"
+    if wavetype == "sine":
+        sine()
+    elif wavetype == "saw":
+        saw()
+    elif wavetype == "square":
+        square()
+    elif wavetype == "rand":
+        rand()
